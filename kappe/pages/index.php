da630c8d91ad5c58d3eda79172ec529f1f0762f6
KaNbI4 <kanych7@gmail.com>
2015-02-19 02:26:26 +0300

    singlepost-adaptive

--- kappe/pages/index.php
+++ kappe/pages/index.php
@@ -4,6 +4,7 @@
 	<meta charset="UTF-8">
 	<title>Document</title>
 	<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,700' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Pinyon+Script' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="./css/build.css">
 </head>
 <body>
